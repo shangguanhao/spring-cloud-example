19498a9b25b87d50ac3b6c95cbe9d42f89553fa6
shangguanhao <1220560544@qq.com>
2019-07-01 19:02:23 +0800

    Spring Cloud Stream的使用；商品和订单服务中使用RabbitMQ实现异步扣库存；将Spring Boot内置的Tomcat改为Jetty

--- product/src/main/java/com/shangguan/product/service/impl/ProductServiceImpl.java
+++ product/src/main/java/com/shangguan/product/service/impl/ProductServiceImpl.java
@@ -1,10 +1,12 @@
 package com.shangguan.product.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,16 @@ import com.shangguan.product.enums.ResultEnum;
 import com.shangguan.product.exception.ProductException;
 import com.shangguan.product.repository.ProductInfoRepository;
 import com.shangguan.product.service.ProductService;
+import com.shangguan.product.utils.JsonUtil;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductInfoRepository productInfoRepository;
+    
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 
     @Override
     public List<ProductInfo> findUpAll() {
@@ -33,12 +39,20 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    public void decreaseStock(List<CartDTO> cartDTOList) {       
+        List<ProductInfo> productInfoList = decreaseStockProcess(cartDTOList);
+        
+        //发送mq消息
+        amqpTemplate.convertAndSend("productInfo", JsonUtil.toJson(productInfoList));
+    }
+    
     @Transactional
-    public void decreaseStock(List<CartDTO> cartDTOList) {
-        for (CartDTO cartDTO : cartDTOList) {
+    public List<ProductInfo> decreaseStockProcess(List<CartDTO> decreaseStockInputList) {
+        List<ProductInfo> productInfoList = new ArrayList<>();
+        for (CartDTO cartDTO: decreaseStockInputList) {
             Optional<ProductInfo> productInfoOptional = productInfoRepository.findById(cartDTO.getProductId());
             //判断商品是否存在
-            if (!productInfoOptional.isPresent()) {
+            if (!productInfoOptional.isPresent()){
                 throw new ProductException(ResultEnum.PRODUCT_NOT_EXIT);
             }
 
@@ -51,8 +65,10 @@ public class ProductServiceImpl implements ProductService {
 
             productInfo.setProductStock(result);
             productInfoRepository.save(productInfo);
+            productInfoList.add(productInfo);
         }
-
+        return productInfoList;
     }
+    
 
 }
